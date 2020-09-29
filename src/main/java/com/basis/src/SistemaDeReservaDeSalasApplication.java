71f881fd4b3f89d1de5cdb1ac2bbf6ff7662b598
Samuel <you@example.com>
2020-09-29 17:20:50 -0300

    repositorio basis

---
+++ src/main/java/com/basis/src/SistemaDeReservaDeSalasApplication.java
@@ -0,0 +1,13 @@
+package com.basis.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservaDeSalasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservaDeSalasApplication.class, args);
+	}
+
+}
