4b9d0ddff5635143fbb147db0ae1a8d2cc2baf52
isaquecosta <isaquecostateotonio@hotmail.com>
2020-10-08 09:50:49 -0300

    Validando Dto's

--- Backend/src/main/java/com/basis/src/servico/dto/EquipamentoDTO.java
+++ Backend/src/main/java/com/basis/src/servico/dto/EquipamentoDTO.java
@@ -3,6 +3,7 @@ package com.basis.src.servico.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
@@ -13,11 +14,11 @@ public class EquipamentoDTO {
     private Integer id;
     @NotNull
     private String nome;
-    @NotNull@Min(0)
+    @NotNull @Min(0)
     private Double preco;
-
+    @Min(0) @Max(1)
     private Integer obrigatorio;
-
+    @Min(1) @Max(2)
     private Integer idTipoEquipamento;
 
 
--- Backend/src/main/java/com/basis/src/servico/dto/SalaDTO.java
+++ Backend/src/main/java/com/basis/src/servico/dto/SalaDTO.java
@@ -5,6 +5,11 @@ import com.basis.src.dominio.SalaEquipamento;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
@@ -12,17 +17,17 @@ import java.util.List;
 public class SalaDTO {
 
     private Integer id;
-
+    @NotNull
     private String descricao;
-
+    @Min(1) @Max(5)
     private Integer idTipoSala;
-
+    @Positive @NotNull
     private Integer capacidadePessoas;
-
+    @NotNull
     private Double precoDiario;
-
+    @Min(0) @Max(1)
     private Integer disponivel;
-
+    @NotNull
     private List<SalaEquipamentoDTO> equipamentos;
 
 }
